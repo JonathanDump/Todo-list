71d4c3ad1b9ce22bdd2e46effe56b46391f6fd11
Jonathan Dump <jbrodii77@gmail.com>
2023-05-27 22:23:11 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./index.html";
+import "./scss/index.scss";
+
+console.log("s222");
+
+// const name = document.querySelector(".name__text");
+
+// document.addEventListener("keydown", (e) => {
+//   let code = e.key;
+//   if (code == "a") {
+//     name.textContent += "1";
+//   } else {
+//     name.textContent += code;
+//   }
+// });
