4b77c0857e34bf5d8abe3310e31029809144bb2f
Jonathan Dump <jbrodii77@gmail.com>
2023-05-28 21:24:43 +0300

    create task and create project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import "./index.html";
 import "./scss/index.scss";
 
-console.log("s222");
+console.log("22");
 
-// const name = document.querySelector(".name__text");
-
-// document.addEventListener("keydown", (e) => {
-//   let code = e.key;
-//   if (code == "a") {
-//     name.textContent += "1";
-//   } else {
-//     name.textContent += code;
-//   }
-// });
+//function to enable add-btn on create form(call it when input is changing)
+function success() {
+  if (document.querySelector(".info-wrapper__content").value === "") {
+    document.querySelector(".form-btn-wrapper__btn-add-task").disabled = true;
+  } else {
+    document.querySelector(".form-btn-wrapper__btn-add-task").disabled = false;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  devtool: "inline-source-map",
   devServer: {
     open: true,
     hot: true,
